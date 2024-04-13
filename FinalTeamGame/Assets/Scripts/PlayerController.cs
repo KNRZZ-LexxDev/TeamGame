d8b4a41b04ba1d3376fbe286fe15827b7d98a914
knrzz <popov0600@gmail.com>
2024-04-13 21:47:25 +0300

    Добавил примиты и скрипт
    Сделал землю и куба, написал скрипт с возможностью перемещения прыжка и ускорения
    Ускорение - L.Shift
    Прыжок - Space
    Движение - A и D

---
+++ FinalTeamGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+    public float acceleration = 10f;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        // Перемещение по горизонтали
+        float moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+
+        // Прыжок
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+
+        // Ускорение
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.A))
+        {
+            rb.velocity -= new Vector2(transform.right.x, 0) * acceleration * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKey(KeyCode.D))
+        {
+            rb.velocity += new Vector2(transform.right.x, 0) * acceleration * Time.deltaTime;
+        }
+    }
+}
