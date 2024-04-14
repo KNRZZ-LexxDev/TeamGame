2c7caf2448000fff0325808d841cbe814dbeaa6c
knrzz <popov0600@gmail.com>
2024-04-14 14:30:11 +0300

    Добавил механику
    Механика батута

---
+++ FinalTeamGame/Assets/Scripts/Spring.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Spring : MonoBehaviour
+{
+    [Header("Упругость батута")]
+    [Space]
+    [SerializeField] private float _force;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.rigidbody.AddForce(Vector2.up * _force, ForceMode2D.Impulse);
+        }
+    }
+}
