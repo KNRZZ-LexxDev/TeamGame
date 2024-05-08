08edd2c706a75bc5fe6c14cdaaa106a738b6c729
LEXX <166923432+KNRZZ-LexxDev@users.noreply.github.com>
2024-05-09 00:29:12 +0300

    BIG BLACK КУРИЦА

---
+++ FinalTeamGame/Assets/Scripts/MaterialScaler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialScaler : MonoBehaviour
+{
+	public float factor = 1;
+
+	void Update()
+	{
+		var mf = GetComponent<MeshFilter>();
+		if (mf != null)
+		{
+			var bounds = mf.mesh.bounds;
+
+			var size = Vector3.Scale(bounds.size, transform.localScale) * factor;
+
+			if (size.y < .001)
+				size.y = size.z;
+
+			GetComponent<Renderer>().material.mainTextureScale = size;
+		}
+	}
+}
+
